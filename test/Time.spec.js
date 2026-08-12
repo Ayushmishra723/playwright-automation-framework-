185c839724af14a0763659fdecd5156362bde84e
Ayush mishra <113902718+Ayushmishra723@users.noreply.github.com>
2026-08-12 21:40:52 +0530

    time changes

---
+++ test/Time.spec.js
@@ -0,0 +1,27 @@
+import{test,expect} from "@playwright/test"
+
+import{login,logout} from "../utils/authhelper.js"
+
+
+
+test.beforeEach(async ({page}) => {
+   await login(page);
+   //await expect(page).toHaveURL(/time/);
+
+
+
+
+});
+
+
+test("to test time " ,async({page}) =>{
+await page.getByRole('link', { name: 'Time' }).click();
+
+console.log("ayush");
+await expect(page).toHaveURL(/time/);
+await page.getByRole('button', { name: 'View' }).first().click();
+
+
+
+
+})
