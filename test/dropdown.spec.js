a93d77d0cbed8fe5bc8634d8f560998cdd69750d
Ayush mishra <113902718+Ayushmishra723@users.noreply.github.com>
2026-08-17 22:48:29 +0530

    now it is correct

--- test/dropdown.spec.js
+++ test/dropdown.spec.js
@@ -8,13 +8,9 @@ await authenticatedPage.getByText("-- Select --").first().click();
   // await authenticatedPage.getByText("Admin", { exact: true }).click();
    //await authenticatedPage.locator("//div[@class='oxd-select-text-input' and text()='Admin']").first().click();
    //await authenticatedPage.getByText("-- Select --").first().click();
-await authenticatedPage
-    .locator("//div[@class='oxd-table-filter-area']//div[2]//div[1]//div[2]//div[1]//div[1]//div[2]//i[1]").click();
 
-await authenticatedPage
-    .getByRole("listbox")
-    .getByText("Admin", { exact: true })
-    .click();
-   await authenticatedPage.pause();
+
+await authenticatedPage.locator("//div[@role='option']//span[text()='Admin']").click();
+   
 
 })
